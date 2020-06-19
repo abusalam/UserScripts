fc56cc6437c645482d293c8d32207a02dcf93d48
Abu Salam Parvez Alam <abusalamparvezalam@gmail.com>
2020-06-19 19:29:31 +0530

    Fixed for WhatsApp Updates

--- COVID-19-WhatsApp-WebBot.user.js
+++ COVID-19-WhatsApp-WebBot.user.js
@@ -19,7 +19,7 @@ function jQueryInclude(callback) {
         var UserScript = document.createElement('script');
         UserScript.textContent = 'window.jQ=jQuery.noConflict(true);'
             + 'var BaseURL = "https://www.malda.gov.in/";'
-            + 'var Version = "v0.0.75";'
+            + 'var Version = "v0.0.76";'
             + '(' + callback.toString() + ')();';
         document.body.appendChild(UserScript);
     }, false);
@@ -697,17 +697,22 @@ jQueryInclude(function () {
             window.webpackJsonp = webpackJsonp;
         }
 
-        if (!window.Store) {
+        if (!window.Store||!window.Store.Msg) {
             (function () {
                 function getStore(modules) {
                     let foundCount = 0;
                     let neededObjects = [
-                        { id: "Store", conditions: (module) => (module.Chat && module.Msg) ? module : null },
-                        { id: "MediaCollection", conditions: (module) => (module.default && module.default.prototype && module.default.prototype.processAttachments) ? module.default : null },
+                        { id: "Store", conditions: (module) => (module.default && module.default.Chat && module.default.Msg) ? module.default : null},
+                        { id: "MediaCollection", conditions: (module) => (module.default && module.default.prototype && (module.default.prototype.processFiles !== undefined||module.default.prototype.processAttachments !== undefined)) ? module.default : null },
                         { id: "MediaProcess", conditions: (module) => (module.BLOB) ? module : null },
+                        { id: "Archive", conditions: (module) => (module.setArchive) ? module : null },
+                        { id: "Block", conditions: (module) => (module.blockContact && module.unblockContact) ? module : null },
+                        { id: "ChatUtil", conditions: (module) => (module.sendClear) ? module : null },
+                        { id: "GroupInvite", conditions: (module) => (module.queryGroupInviteCode) ? module : null },
                         { id: "Wap", conditions: (module) => (module.createGroup) ? module : null },
                         { id: "ServiceWorker", conditions: (module) => (module.default && module.default.killServiceWorker) ? module : null },
                         { id: "State", conditions: (module) => (module.STATE && module.STREAM) ? module : null },
+                        { id: "_Presence", conditions: (module) => (module.setPresenceAvailable && module.setPresenceUnavailable) ? module : null },
                         { id: "WapDelete", conditions: (module) => (module.sendConversationDelete && module.sendConversationDelete.length == 2) ? module : null },
                         { id: "Conn", conditions: (module) => (module.default && module.default.ref && module.default.refTTL) ? module.default : null },
                         { id: "WapQuery", conditions: (module) => (module.queryExist) ? module : ((module.default && module.default.queryExist) ? module.default : null) },
@@ -715,8 +720,32 @@ jQueryInclude(function () {
                         { id: "OpenChat", conditions: (module) => (module.default && module.default.prototype && module.default.prototype.openChat) ? module.default : null },
                         { id: "UserConstructor", conditions: (module) => (module.default && module.default.prototype && module.default.prototype.isServer && module.default.prototype.isUser) ? module.default : null },
                         { id: "SendTextMsgToChat", conditions: (module) => (module.sendTextMsgToChat) ? module.sendTextMsgToChat : null },
-                        { id: "SendSeen", conditions: (module) => (module.sendSeen) ? module.sendSeen : null },
-                        { id: "sendDelete", conditions: (module) => (module.sendDelete) ? module.sendDelete : null }
+                        { id: "ReadSeen", conditions: (module) => (module.sendSeen) ? module : null },
+                        { id: "sendDelete", conditions: (module) => (module.sendDelete) ? module.sendDelete : null },
+                        { id: "addAndSendMsgToChat", conditions: (module) => (module.addAndSendMsgToChat) ? module.addAndSendMsgToChat : null },
+                        { id: "sendMsgToChat", conditions: (module) => (module.sendMsgToChat) ? module.sendMsgToChat : null },
+                        { id: "Catalog", conditions: (module) => (module.Catalog) ? module.Catalog : null },
+                        { id: "bp", conditions: (module) => (module.default&&module.default.toString().includes('bp_unknown_version')) ? module.default : null },
+                        { id: "MsgKey", conditions: (module) => (module.default&&module.default.toString().includes('MsgKey error: obj is null/undefined')) ? module.default : null },
+                        { id: "Parser", conditions: (module) => (module.convertToTextWithoutSpecialEmojis) ? module.default : null },
+                        { id: "Builders", conditions: (module) => (module.TemplateMessage && module.HydratedFourRowTemplate) ? module : null },
+                        { id: "Me", conditions: (module) => (module.PLATFORMS && module.Conn) ? module.default : null },
+                        { id: "CallUtils", conditions: (module) => (module.sendCallEnd && module.parseCall) ? module : null },
+                        { id: "Identity", conditions: (module) => (module.queryIdentity && module.updateIdentity) ? module : null },
+                        { id: "MyStatus", conditions: (module) => (module.getStatus && module.setMyStatus) ? module : null },
+                        { id: "ChatStates", conditions: (module) => (module.sendChatStatePaused && module.sendChatStateRecording && module.sendChatStateComposing) ? module : null },
+                        { id: "GroupActions", conditions: (module) => (module.sendExitGroup && module.localExitGroup) ? module : null },
+                        { id: "Features", conditions: (module) => (module.FEATURE_CHANGE_EVENT && module.features) ? module : null },
+                        { id: "MessageUtils", conditions: (module) => (module.storeMessages && module.appendMessage) ? module : null },
+                        { id: "WebMessageInfo", conditions: (module) => (module.WebMessageInfo && module.WebFeatures) ? module.WebMessageInfo : null },
+                        { id: "createMessageKey", conditions: (module) => (module.createMessageKey && module.createDeviceSentMessage) ? module.createMessageKey : null },
+                        { id: "Participants", conditions: (module) => (module.addParticipants && module.removeParticipants && module.promoteParticipants && module.demoteParticipants) ? module : null },
+                        { id: "WidFactory", conditions: (module) => (module.isWidlike && module.createWid && module.createWidFromWidLike) ? module : null },
+                        { id: "Base", conditions: (module) => (module.setSubProtocol && module.binSend && module.actionNode) ? module : null },
+                        { id: "Versions", conditions: (module) => (module.loadProtoVersions && module.default["15"] && module.default["16"] && module.default["17"]) ? module : null },
+                        { id: "Sticker", conditions: (module) => (module.default && module.default.Sticker) ? module.default.Sticker : null },
+                        { id: "MediaUpload", conditions: (module) => (module.default && module.default.mediaUpload) ? module.default : null },
+                        { id: "UploadUtils", conditions: (module) => (module.default && module.default.encryptAndUpload) ? module.default : null }
                     ];
                     for (let idx in modules) {
                         if ((typeof modules[idx] === "object") && (modules[idx] !== null)) {
@@ -724,6 +753,7 @@ jQueryInclude(function () {
                             if ((typeof first === "object") && (first.exports)) {
                                 for (let idx2 in modules[idx]) {
                                     let module = modules(idx2);
+                                    // console.log("TCL: getStore -> module", module ? Object.getOwnPropertyNames(module.default || module).filter(item => typeof (module.default || module)[item] === 'function').length ? module.default || module : "":'')
                                     if (!module) {
                                         continue;
                                     }
@@ -752,30 +782,17 @@ jQueryInclude(function () {
                                 window.Store.sendMessage = function (e) {
                                     return window.Store.SendTextMsgToChat(this, ...arguments);
                                 }
+                                if(window.Store.MediaCollection) window.Store.MediaCollection.prototype.processFiles = window.Store.MediaCollection.prototype.processFiles || window.Store.MediaCollection.prototype.processAttachments;
                                 return window.Store;
                             }
                         }
                     }
                 }
+                const parasite = `parasite${Date.now()}`
+                // webpackJsonp([], { [parasite]: (x, y, z) => getStore(z) }, [parasite]);
+                if (typeof webpackJsonp === 'function') webpackJsonp([], {[parasite]: (x, y, z) => getStore(z)}, [parasite]);
+                else webpackJsonp.push([[parasite],{[parasite]: (x, y, z) => getStore(z)},[[parasite]]]);
 
-                // webpackJsonp([], { 'parasite': (x, y, z) => getStore(z) }, ['parasite']);
-
-                /**
-                 * Code update
-                 */
-                if (typeof webpackJsonp === 'function') {
-                    webpackJsonp([], { 'parasite': (x, y, z) => getStore(z) }, ['parasite']);
-                } else {
-                    webpackJsonp.push([
-                        ['parasite'],
-                        {
-                            parasite: function (o, e, t) {
-                                getStore(t);
-                            }
-                        },
-                        [['parasite']]
-                    ]);
-                }
             })();
         }
 
@@ -1195,6 +1212,6 @@ jQueryInclude(function () {
         window.addEventListener("pageunload", window.WAPI._unloadInform, false);
         window.WappBot.configWappBot.updateOptions(WappBot.configWappBot.covidQueries);
         console.log("Application Ready");
-        jQ("._2LKlu").before('<div id="covid-tag" style="padding-bottom:25px;font-size:15px;"><div style="padding-bottom:15px;font-size:25px;color:#1B9A59">COVID-19 Helpline Malda</div><p>' + Version + '</p></div>');
+        jQ("._2bBPp").before('<div id="covid-tag" style="padding-bottom:25px;font-size:15px;"><div style="padding-bottom:15px;font-size:25px;color:#1B9A59">COVID-19 Helpline Malda</div><p>' + Version + '</p></div>');
     }, 15000);
 });
